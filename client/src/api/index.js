c520f3cb9d1a4e25e708b8c0caf6c4cb78ee3c77
Prakash <127179011+ProPrak01@users.noreply.github.com>
2024-04-13 13:54:57 +0530

    c

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5003' });
+const API = axios.create({ baseURL: 'http://localhost:5004' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-
+import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
-
+dotenv.config({ path: './values.env' });
 const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
@@ -16,8 +15,8 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://ph1010ishard:CiIa22qt8pmCXJrD@socialmediacluster.usym5ew.mongodb.net/';
-const PORT = process.env.PORT|| 5003;
+const CONNECTION_URL= process.env.CONNECTION_URL
+const PORT= process.env.PORT;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
