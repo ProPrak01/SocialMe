1a514d8f61b8c270fe4ac07b7a2e2a0b50d21344
Prakash <127179011+ProPrak01@users.noreply.github.com>
2024-04-13 14:35:28 +0530

    chanhes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors({
+  origin: ['https://social-me-server.vercel.app', 'http://localhost:3000'], // add more origins if needed
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.get("/", (req, res) => {
   res.send("Hello world home");
 });
