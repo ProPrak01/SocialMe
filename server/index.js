3bf925733e1df8875dcca67196cc44bac52b8e11
Prakash <127179011+ProPrak01@users.noreply.github.com>
2024-04-13 14:09:26 +0530

    EW

--- server/index.js
+++ server/index.js
@@ -5,18 +5,21 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
-dotenv.config({ path: './values.env' });
 const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors({
+  origin:["https://social-me-server-700m9wv3w-proprak01s-projects.vercel.app/"],
+  method:["POST","GET"],
+  credentials:true
+}));
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL= process.env.CONNECTION_URL
-const PORT= process.env.PORT;
+const CONNECTION_URL= 'mongodb+srv://ph1010ishard:CiIa22qt8pmCXJrD@socialmediacluster.usym5ew.mongodb.net/'
+const PORT= 5004;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
