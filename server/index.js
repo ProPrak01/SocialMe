210b0a0ff49e9f7999910fae5ef0ce5827060d59
Prakash <127179011+ProPrak01@users.noreply.github.com>
2024-04-13 14:22:13 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -9,11 +9,7 @@ const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors({
-  origin:["https://social-me-server.vercel.app"],
-  method:["POST","GET"],
-  credentials:true
-}));
+app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
