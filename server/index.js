02294ea5932e9392d2f1c3cf03dd6132a72c7fa8
Prakash <127179011+ProPrak01@users.noreply.github.com>
2024-04-13 14:25:10 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -10,10 +10,9 @@ const app = express();
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-app.get('/',()=>{
-    alert("server runnning");
-    
-})
+app.get("/", (req, res) => {
+  res.send("Hello world home");
+});
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
